45985108016a25df313e2adc6dda9b63c7c117cc
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-06-08 11:50:08 +0200

    add span balise to emojis

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Header = ({}) => {
+const Header = () => {
   return (
     <div>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textDecorationLine: `none` }}>
-          🔅BenoCharlo 🔅
+          <span role="img" aria-label="light">
+            🔅
+          </span>
+          BenoCharlo{" "}
+          <span role="img" aria-label="light">
+            🔅
+          </span>
         </Link>
         <ul style={{ listStyle: `none`, float: `right` }}>
           <Link to="/about" style={{ textDecorationLine: `none` }}>
