9ca45af2e1d3a05cc95e91ee1977045140142d41
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-06-08 06:52:07 +0200

    add custom typography styling

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,19 @@
 import Typography from "typography"
 
-const typography = new Typography()
-
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Helvetica", "georgia"],
+  bodyFontFamily: ["Helvetica", "sans-serif"],
+  googleFonts: [
+    {
+      name: "Helvetica",
+      styles: ["800", "400"],
+    },
+    {
+      name: "Open Sans",
+      styles: ["400", "300", "200"],
+    },
+  ],
+})
 export default typography
-export const rhythm = typography.rhythm
