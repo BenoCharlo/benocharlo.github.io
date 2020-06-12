7f070f1a2e2d86b8a12c62de4d3488671085bc4f
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-06-12 10:36:26 +0200

    remove usestaticquery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Moment from "react-moment"
