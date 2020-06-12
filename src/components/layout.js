d6d415221571709a61bb1f5b6a60ffd1f79a26cf
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-06-12 13:08:53 +0200

    add SEO data

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,29 @@
 import React from "react"
 import Header from "./header"
+import SEO from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            url
+            image
+          }
+        }
+      }
+    `
+  )
   return (
     <div
       className="container"
       style={{ margin: `2rem auto`, maxWidth: 850, padding: `0 1rem` }}
     >
+      <SEO {...data.site.siteMetadata} />
       <Header />
       {children}
     </div>
