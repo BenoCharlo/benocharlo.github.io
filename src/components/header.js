dfb14097ddc571294cff493389b8e4cd522b8a0e
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-06-17 10:52:30 +0200

    add navbar and hamburger button styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,148 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Linkedin from "../images/svg/LinkedinSVG"
 import Github from "../images/svg/GithubSVG"
 import fadeInUpward from "../styles/_animations/fadeInUpward"
 import styled from "styled-components"
+import "../styles/hamburgers.css"
 
 const HeaderLinks = styled.ul`
   transition: ${props => props.theme.transition};
+  liststyle: none;
+  margin-left: 0;
   .a {
     animation: ${fadeInUpward} 1s;
     transition: 0.2s;
   }
   .a:hover {
-    transoform: translateY(-10px);
+    transform: translateY(-10px);
+  }
+`
+
+const HeaderWrapper = styled.header`
+  position: fixed;
+  z-index: 5;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 70px;
+  background-color: white;
+  transition: background-color ${props => props.theme.transition},
+    box-shadow 0.3s;
+  box-shadow: ${props => (props.isScrolled ? "0px 0px 10px #4d4d4d" : "none")};
+  display: flex;
+
+  .navContainer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 0.5rem auto;
+    width: 50%;
+
+      a{
+        font-size: 1.2rem;
+        font-weight: 600;
+        text-decoration: none;
+        outline: none;        
+      }
+      a+a {
+        margin-left: 5px;
+      }
+      .activeElement{
+        animation: ${fadeInUpward} "0.5s";
+        transition: 0.2s;
+        &:hover {
+          transform: translateY(-10px)
+        }
+      }
+      .activeText{
+        border-bottom: 3px solid ${props => props.theme.textColor};
+      }
+      h2 {
+        color: ${props => props.theme.textColor};
+        font-size: 0.563rem;
+        text-align: center;
+        letter-spacing: 4.4px;
+        // margin: 0;
+      }
+    }
+    nav {
+      margin: 40px 0 0 370px;
+      margin-right: 0.5rem auto;
+      ul {
+        display: flex;
+        flex-direction: row;
+        list-style-type: none;
+        li {
+          margin-right: 0;
+          list-style-type: none;
+          justify-content: space-around;
+          a {
+            // font-family: "Montserrat", "Helvetica", "sans-serif";
+            font-weight: 600;
+            font-size: 1.2rem;
+            letter-spacing: 1px;
+            text-decoration: none;
+            padding: 0 2px 6px;
+            border-bottom: 3px solid transparent;
+            margin: 0;
+          }
+          // a:hover {
+          //   // transform: translateY(-10px);
+          //   border-bottom: 3px solid ${props => props.theme.textColor};
+          // }
+          .activePage {
+            border-bottom: 3px solid ${props => props.theme.textColor};
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 980px) {
+    .navContainer {
+      nav {
+        display: none;
+      }
+    }
+  }
+  @media (min-width: 880px) {
+    height: 80px;
+    .navContainer {
+      height: 80px;
+      #logo {
+        width: 205px;
+        height: auto;
+        .headerLogoSVG {
+          transition: ${props => props.theme.transition};
+          fill: ${props => props.theme.textColor};
+          width: 100%;
+        }
+        h2 {
+          font-size: 0.69rem;
+          letter-spacing: 5.8px;
+        }
+      }
+    }
   }
 `
 
 export default function Header() {
+  let [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 30 ||
+        document.documentElement.scrollTop > 30
+      ) {
+        setIsScrolled((isScrolled = true))
+      } else {
+        setIsScrolled((isScrolled = false))
+      }
+    })
+  }, [])
+
   const data = useStaticQuery(
     graphql`
       query {
@@ -32,16 +158,16 @@ export default function Header() {
 
   return (
     <div>
-      <header style={{ marginBottom: `1.5rem`, color: `black` }}>
-        {/* <ul style={{ listStyle: `none`, marginLeft: 0 }}> */}
-        <HeaderLinks style={{ listStyle: `none`, marginLeft: 0 }}>
-          <Link to="/" style={{ textDecorationLine: `none` }}>
+      <HeaderWrapper isScrolled={isScrolled}>
+        <div className="navContainer">
+          <Link to="/" acitveClassName="activeText">
             <span role="img" aria-label="sunlight">
               🔅
             </span>
             BenoCharlo{" "}
           </Link>{" "}
           <a
+            className="activeElement"
             target="_blank"
             href={data.site.siteMetadata.authorLinkedIn}
             aria-label="My LinkedIn profile"
@@ -50,6 +176,7 @@ export default function Header() {
             <Linkedin />
           </a>{" "}
           <a
+            className="activeElement"
             target="_blank"
             href={data.site.siteMetadata.authorGithub}
             rel="noopener noreferrer"
@@ -57,16 +184,17 @@ export default function Header() {
           >
             <Github />
           </a>
-          <ul style={{ listStyle: `none`, float: `right` }}>
-            <Link to="/about" style={{ textDecorationLine: `none` }}>
-              About
-            </Link>{" "}
-            {/* <Link to="/certifications" style={{ textDecorationLine: `none` }}>
-              Certifs
-            </Link> */}
-          </ul>
-        </HeaderLinks>
-      </header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/about" activeClassName="activePage">
+                  About
+                </Link>{" "}
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </HeaderWrapper>
     </div>
   )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,60 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 import Header from "./header"
 import SEO from "../components/seo"
 import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
+import MobileNav from "./mobilenav"
+
+const LayoutWrapper = styled.div`
+  margin: 0;
+  transition: ${props => props.theme.transition};
+  background-color: ${props => props.theme.bgColor};
+  padding: 15px;
+  min-height: 100vh;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a,
+  li {
+    transition: ${props => props.theme.transition};
+    color: ${props => props.theme.textColor};
+    background-color: ${props => props.theme.bgColor};
+  }
+
+  main {
+    transition: ${props => props.theme.transition};
+    background-color: ${props => props.theme.bgColor};
+    width: 50%;
+    padding: 12px 0px 24px;
+    margin: 60px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    //align-items: center;
+    // max-width: 10000px;
+  }
+`
 
 export default function Layout({ children }) {
+  let [mobileNavIsOpen, toggleMobileNav] = useState(false)
+
+  const mobileNavAction = () => {
+    toggleMobileNav(
+      mobileNavIsOpen ? (mobileNavIsOpen = false) : (mobileNavIsOpen = true)
+    )
+  }
+
+  // adds smooth scrolling
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // eslint-disable-next-line global-require
+      require("smooth-scroll")('a[href*="#"]')
+    }
+  })
   const data = useStaticQuery(
     graphql`
       query {
@@ -19,13 +70,16 @@ export default function Layout({ children }) {
     `
   )
   return (
-    <div
-      className="container"
-      style={{ margin: `2rem auto`, maxWidth: 850, padding: `0 1rem` }}
-    >
+    // <div
+    //   className="container"
+    //   style={{ margin: `2rem auto`, maxWidth: 850, padding: `0 1rem` }}
+    // >
+    <LayoutWrapper>
       <SEO {...data.site.siteMetadata} />
       <Header />
-      {children}
-    </div>
+      <MobileNav mobileNavIsOpen={mobileNavIsOpen} action={mobileNavAction} />
+      <main>{children}</main>
+    </LayoutWrapper>
+    // </div>
   )
 }
