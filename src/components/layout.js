efe88e70d6d15dd39d73a34c48a8b6fb2403d360
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-06-17 11:06:32 +0200

    delete useContext

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import Header from "./header"
 import SEO from "../components/seo"
 import { graphql, useStaticQuery } from "gatsby"
