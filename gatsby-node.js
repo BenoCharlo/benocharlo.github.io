3bdcf48c350997d74e16fd0bff34e4e27e77e61e
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-07-11 09:26:57 +0200

    add gatsby img/relative img plugin sharp

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,12 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const { fmImagesToRelative } = require("gatsby-remark-relative-images")
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
+    fmImagesToRelative(node)
     createNodeField({
       node,
       name: `slug`,
