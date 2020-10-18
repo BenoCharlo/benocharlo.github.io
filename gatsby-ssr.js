49ef7de10be16ae658c4ee9ac3f61d1f01ca481e
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-10-18 10:45:21 +0200

    correct ThemeContext in gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ThemeContextWrapper from "./src/utils/ThemeContext"
+import ThemeContextWrapper from "./src/utils/themeContext"
 
 // provide theme state to entire app
 export const wrapRootElement = ({ element }) => (
