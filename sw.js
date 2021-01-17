b367078384c2dd0afde13e5674f4dec9cacaca01
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2021-01-17 11:16:14 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5227d4cf6765fb250162.js"
+    "url": "webpack-runtime-c4644b2016325f163d39.js"
   },
   {
     "url": "styles.2492213207740c4b8cb5.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7bc88b40ddb970b7f986db0213c2ffd1"
+    "revision": "117cde18d4f092f42d20476d4f5c0da5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
